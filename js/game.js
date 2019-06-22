d96cc813f3ab60bef94860be39f2a110fbe54f8a
myronmcm <myron@myronmcmillin.com>
2019-06-22 16:43:09 -0700

    Structural Changes. Moved levels to external file.

--- js/game.js
+++ js/game.js
@@ -2,37 +2,8 @@
 // Todo: Move hardcoded strings to variables.
 // Move QuerySelectors to variables
 //Reset colors on level change
-
-const levels = [
-  {
-    name: "level 1",
-    description: "Use the named color for this paint. Hint: It is a fruit",
-    targetColor: "rgba(0, 255, 0, 1)",
-    targetType: "named"
-  },
-  {
-    name: "level 2",
-    description:
-      "Use the RGB color values for this level. Remember the syntax is rgb(0-255,0-255,0-255)",
-    targetColor: "rgba(255, 255, 0, 1)",
-    targetType: "rgb"
-  },
-  {
-    name: "level 3",
-    description:
-      "Use the HEX value for this level. Remember the syntax is rgb(0-255,0-255,0-255)",
-    targetColor: "rgba(0, 255, 0, 1)",
-    targetType: "hex"
-  },
-  {
-    name: "level 4",
-    description: "Something something RGBA, alpha channel blah blah.",
-    targetColor: "rgba(120, 255, 68, 0.5)",
-    targetType: "rgba"
-  }
-];
-
 //Todo: Better way to manage game state
+
 let currentLevel = 0; //Sorry, I don't know an ES6 singletony pattern yet
 const maxLevel = levels.length;
 
---
+++ js/levels.js
@@ -0,0 +1,28 @@
+const levels = [
+  {
+    name: "level 1",
+    description: "Use the named color for this paint. Hint: It is a fruit",
+    targetColor: "rgba(0, 255, 0, 1)",
+    targetType: "named"
+  },
+  {
+    name: "level 2",
+    description:
+      "Use the RGB color values for this level. Remember the syntax is rgb(0-255,0-255,0-255)",
+    targetColor: "rgba(255, 255, 0, 1)",
+    targetType: "rgb"
+  },
+  {
+    name: "level 3",
+    description:
+      "Use the HEX value for this level. Remember the syntax is rgb(0-255,0-255,0-255)",
+    targetColor: "rgba(0, 255, 0, 1)",
+    targetType: "hex"
+  },
+  {
+    name: "level 4",
+    description: "Something something RGBA, alpha channel blah blah.",
+    targetColor: "rgba(120, 255, 68, 0.5)",
+    targetType: "rgba"
+  }
+];
